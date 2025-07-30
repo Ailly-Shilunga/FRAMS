6cf79dddcd5236b571b2ced8a67e2036969bf372
Ailly Shilunga <jaillyshilunga@gmail.com>
2025-07-30 14:33:12 +0200

    changes

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
 console.log("Hello from FRAMS!");
+git addconst express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to FRAMS!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});vvss
